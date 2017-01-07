f2058b248261bcc21749fa0d9b1ab617ade9b38c
Kyle <kyle@eatstreet.com>
2017-01-06 18:29:48 -0600

    add guages man

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@ angular
     'ngRiffle',
     'nvd3',
     'ngTable',
+    'angularjs-gauge',
   ])
 
   .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
